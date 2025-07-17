9955e1e64dc7d7119927608415caccbbf431a64a
Bijoy99roy <bijoy2014amp@gmail.com>
2025-07-18 00:07:02 +0530

    Initial commit with basic code 🖥️

---
+++ src/index.ts
@@ -0,0 +1,115 @@
+import Client, {
+  SubscribeRequest,
+  CommitmentLevel,
+  SubscribeRequestFilterAccounts,
+  SubscribeRequestFilterTransactions,
+} from "@triton-one/yellowstone-grpc";
+require("dotenv").config();
+import bs58 from "bs58";
+console.log(process.env.YELLOWSTONE_GRPC_URL);
+const client = new Client(
+  process.env.YELLOWSTONE_GRPC_URL!,
+  undefined,
+  undefined
+);
+
+async function startIndexer() {
+  const version = await client.getVersion(); // gets the version information
+  console.log(version);
+  const stream = await client.subscribe();
+  stream.on("data", (data) => {
+    console.log("data", data);
+    console.log(
+      "-----------------------------------------------------------------------------------------------------"
+    );
+    if (data.block) {
+      if (data.block.transactions.length) {
+        for (let i = 0; i < data.block.transactions.length - 1; i++) {
+          if (!data.block.transactions[i].isVote) {
+            console.log(
+              "Signature",
+              bs58.encode(data.block.transactions[i].signature)
+            );
+            console.log(
+              "Trnasaction Signature",
+              bs58.encode(data.block.transactions[i].transaction.signatures[0])
+            );
+            console.log(
+              "HEADER",
+              data.block.transactions[i].transaction.message.header
+            );
+            console.log(
+              "ACCOUNT KEYS",
+              data.block.transactions[i].transaction.message.accountKeys[0]
+            );
+            console.log(
+              "INSTRUNCTIONS ACCOUNTS",
+              bs58.encode(
+                data.block.transactions[i].transaction.message.instructions[0]
+                  .accounts
+              )
+            );
+            console.log(
+              "INSTRUNCTIONS DATA",
+              bs58.encode(
+                data.block.transactions[i].transaction.message.instructions[0]
+                  .data
+              )
+            );
+            console.log("transaction", data.block.transactions[i]);
+          }
+        }
+      }
+    }
+    console.log(
+      "********************************************************************************************************"
+    );
+  });
+  stream.on("error", (err) => console.error("stream error:", err));
+  //   const solendFilter: SubscribeRequestFilterTransactions = {
+  //     serum: {
+  //       vote: false,
+  //     },
+  // }
+  //   };
+
+  const request: SubscribeRequest = {
+    slots: {},
+    accounts: {},
+    transactions: {
+      alltxs: {
+        vote: false,
+        failed: false,
+        signature: undefined,
+        accountInclude: [],
+        accountExclude: [],
+        accountRequired: [],
+      },
+    },
+    blocks: {
+      blocks: {
+        includeTransactions: true,
+        includeAccounts: false,
+        accountInclude: [],
+      },
+    },
+    transactionsStatus: {},
+    entry: {},
+    blocksMeta: {},
+    accountsDataSlice: [],
+  };
+  await new Promise<void>((resolve, reject) => {
+    stream.write(request, (err: Error) => {
+      if (err === null || err === undefined) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  }).catch((reason) => {
+    console.error(reason);
+    throw reason;
+  });
+}
+
+startIndexer();
